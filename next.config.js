3627913359efefc054c314f54da5061048b77dc9
Nathen-Smith <nathencsmith@gmail.com>
2022-08-07 11:09:45 -0700

    deploy setup

--- next.config.js
+++ next.config.js
@@ -3,5 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  assetPrefix: isProd ? "https://nathen-smith.github.io/blog" : "",
+  nextConfig,
+  basePath: "/blog",
+  images: {
+    loader: "akamai",
+    path: "",
+    domains: ["http://i.annihil.us/u/prod/marvel/i/mg"],
+  },
+}
