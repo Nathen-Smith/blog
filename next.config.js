cb1c0c52aa2fdcc038102e240c6817903e48f549
Nathen-Smith <nathencsmith@gmail.com>
2022-08-07 11:19:07 -0700

    tailwind + basepath

--- next.config.js
+++ next.config.js
@@ -3,16 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
-const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  ...nextConfig,
-  assetPrefix: isProd ? "https://nathen-smith.github.io/blog" : "",
-  nextConfig,
   basePath: "/blog",
-  images: {
-    loader: "akamai",
-    path: "",
-    domains: ["http://i.annihil.us/u/prod/marvel/i/mg"],
-  },
 }
