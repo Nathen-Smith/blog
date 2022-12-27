4d1e7959b26b03086d5b28987f2efe669ba28bf0
Nathen-Smith <nathencsmith@gmail.com>
2022-12-27 12:51:32 -0800

    test: use js to change docElem color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,14 +10,16 @@ export default function Document() {
             __html: `
               if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
                 document.documentElement.classList.add('dark');
+                document.documentElement.style.backgroundColor = 'rgb(39 39 42)';
               } else {
                 document.documentElement.classList.remove('dark');
+                document.documentElement.style.backgroundColor = 'rgb(244 244 245)';
               }
             `,
           }}
         />
       </Head>
-      <body>
+      <body className="bg-zinc-100 transition-colors dark:bg-zinc-800 sm:bg-white sm:dark:bg-zinc-900">
         <Main />
         <NextScript />
       </body>
