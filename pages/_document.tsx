5d73b2bb224f908f8a424c2c2f1f9c26c6b8d369
Nathen-Smith <nathencsmith@gmail.com>
2022-12-27 12:39:03 -0800

    revert: html doc classname styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,18 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html
-      lang="en"
-      className="bg-zinc-100 transition-colors dark:bg-zinc-800 sm:bg-white sm:dark:bg-zinc-900"
-    >
+    <Html lang="en">
       <Head>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                document.documentElement.classList.add('dark')
+                document.documentElement.classList.add('dark');
               } else {
-                document.documentElement.classList.remove('dark')
+                document.documentElement.classList.remove('dark');
               }
             `,
           }}
