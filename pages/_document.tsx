18d205f4a68df867e1c63f11c523fff636d82c74
Nathen-Smith <nathencsmith@gmail.com>
2022-12-27 14:35:08 -0800

    fix: html styling in globals

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en" className="transition-colors">
+    <Html lang="en">
       <Head>
         <script
           dangerouslySetInnerHTML={{
