40914542e41c2d6a4a826e26477d7312fe33cc57
Nathen Smith <nathencsmith@gmail.com>
2023-01-23 17:20:03 -0800

    fix[NS-26]: smooth transitions, footer content aligning (#72)
    * fix: smooth transitions, footer content aligning
    
    * style: fix spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="mx-auto mt-auto w-full max-w-[65ch] px-[14px] pb-[14px] text-center sm:px-0 sm:pb-8">
+    <footer className="mx-auto mt-auto w-full max-w-[65ch] px-[14px] pb-[14px] sm:px-0 sm:pb-8">
       <div className="border-t-[1px] border-zinc-500 text-zinc-500 transition-colors dark:border-zinc-400 dark:text-zinc-400">
-        <div className="float-left">
+        <div className="sm:float-left">
           © 2022 by Nathen Smith. All rights reserved.
         </div>
         <a
-          className="float-none font-medium text-black underline transition-colors dark:text-white sm:float-right"
+          className="font-medium text-black underline transition-colors dark:text-white sm:float-right"
           href="https://github.com/Nathen-Smith/blog"
         >
           Source code
