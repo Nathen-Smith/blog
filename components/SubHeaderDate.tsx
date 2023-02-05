b8b1da9c0922d2636b6e0b8ada4d0c1ea6686e60
Nathen Smith <nathencsmith@gmail.com>
2023-02-05 12:41:21 -0800

    NS-32 shortened date (#74)
    * feat: get estimated time for home page & UI
    
    * fix: no hover on tablet, prepare refactor of subtitle
    
    * fix: responsiveness
    
    * feat: omit year and use MMM if curr year
    
    * feat: omit year and use MMM if curr year

--- components/SubHeaderDate.tsx
+++ components/SubHeaderDate.tsx
@@ -5,15 +5,25 @@ interface DateProps {
   dateString: string;
   inline?: boolean;
 }
-export default function Date({ dateString, inline = false }: DateProps) {
+export default function SubHeaderDate({
+  dateString,
+  inline = false,
+}: DateProps) {
   const date = parseISO(dateString);
+  const currYear = new Date().getFullYear();
+
   return (
     <div
       className={`${
         inline ? 'inline' : 'block'
       } font-['SF_Compact_Text','SF_Pro_Text',-apple-system,BlinkMacSystemFont,'Inter','Segoe_UI',Roboto,Helvetica,Arial,sans-serif,'Apple_Color_Emoji','Segoe_UI_Emoji','Segoe_UI_Symbol'] text-sm font-medium uppercase text-zinc-600 dark:text-zinc-400`}
     >
-      <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+      <time dateTime={dateString}>
+        {format(
+          date,
+          date.getFullYear() === currYear ? 'LLL d' : 'LLLL d, yyyy',
+        )}
+      </time>
     </div>
   );
 }
