44c58cc9a7af0f755c873e390371ae3993626efa
Nathen-Smith <nathencsmith@gmail.com>
2023-02-06 21:03:52 -0800

    feat: mdx conversion

--- lib/posts.ts
+++ lib/posts.ts
@@ -3,24 +3,21 @@ import path from 'path';
 
 import matter from 'gray-matter';
 import { remark } from 'remark';
-import html from 'remark-html';
 import remarkMdx from 'remark-mdx';
 
 const POSTS_DIRECTORY = path.join(process.cwd(), 'posts');
 const WPM = 238;
-const SHOW_MDX = false;
-const filterFn = (fileName: string) => SHOW_MDX || fileName.endsWith('md');
 
-export async function getAllMdxIds() {
-  const fileNames = fs.readdirSync(POSTS_DIRECTORY);
-  return fileNames.filter(filterFn).map((fileName) => ({
-    params: {
-      id: fileName.replace(/\.mdx$/, ''),
-    },
-  }));
+export interface PostData {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  estimatedTime: number;
+  contentMdx: string;
 }
 
-export async function getMdxPostData(id: string) {
+export async function getPostData(id: string): Promise<PostData> {
   const fullPath = path.join(POSTS_DIRECTORY, `${id}.mdx`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
@@ -42,51 +39,10 @@ export async function getMdxPostData(id: string) {
     contentMdx,
     estimatedTime,
     ...matterResult.data, // title, description, date
-  };
-}
-
-export interface PostMetaData {
-  title: string;
-  description: string;
-  date: string;
-}
-
-export interface PostData extends PostMetaData {
-  id: string;
-  contentHtml: string;
-  estimatedTime: number;
-}
-
-export interface SortedPostData extends PostMetaData {
-  id: string;
-  estimatedTime: number;
-}
-
-export async function getPostData(id: string): Promise<PostData> {
-  const fullPath = path.join(POSTS_DIRECTORY, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
-
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
-  const contentHtml = processedContent.toString();
-
-  // Estimate the time it takes to read this post
-  const numWords = contentHtml.trim().split(/\s+/).length;
-  const estimatedTime = Math.ceil(numWords / WPM);
-
-  // Combine the data with the id and contentHtml
-  return {
-    id,
-    contentHtml,
-    estimatedTime,
-    ...matterResult.data,
   } as PostData;
 }
+
+export type SortedPostData = Omit<PostData, 'contentMdx'>;
 
 export default async function getSortedPostsData(): Promise<
   Array<SortedPostData>
@@ -95,9 +51,9 @@ export default async function getSortedPostsData(): Promise<
   const fileNames = fs.readdirSync(POSTS_DIRECTORY);
 
   const allPostsData = await Promise.all(
-    fileNames.filter(filterFn).map(async (fileName) => {
-      // Remove ".md" from file name to get id
-      const id = fileName.replace(/\.md$/, '');
+    fileNames.map(async (fileName) => {
+      // Remove ".mdx" from file name to get id
+      const id = fileName.replace(/\.mdx$/, '');
 
       const { date, title, estimatedTime } = await getPostData(id);
 
@@ -140,10 +96,9 @@ export function getAllPostIds() {
   //   }
   // ]
 
-  // x.replace(/\.[^/.]+$/, "")
-  return fileNames.filter(filterFn).map((fileName) => ({
+  return fileNames.map((fileName) => ({
     params: {
-      id: fileName.replace(/\.md$/, ''),
+      id: fileName.replace(/\.mdx$/, ''),
     },
   }));
 }
