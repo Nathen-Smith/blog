b022b1e611f1ae9931a6085b6e412c1166c14a2c
Nathen Smith <nathencsmith@gmail.com>
2023-02-10 16:28:45 -0800

    fix[NS-38]: prose-wrap for mdx files (#78)
    * fix: prose-wrap for mdx files
    
    * fix: prose-wrap always

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,11 +1,17 @@
 import TweetEmbed from 'react-tweet-embed';
 import useDarkMode from '../hooks/useDarkMode';
 
+function Loading() {
+  return <h4 className="text-white">Loading</h4>;
+}
+
 export default function Tweet({ id }: { id: string }) {
   const { isDark } = useDarkMode();
   return (
     <TweetEmbed
       tweetId={id}
+      className="text-white"
+      placeholder={<Loading />}
       options={{ theme: isDark ? 'dark' : 'light', align: 'center' }}
     />
   );
