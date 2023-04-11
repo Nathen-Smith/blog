6bd2e633cfd50d3b5bcf6437634c3dd1b05f790f
Nathen-Smith <nathencsmith@gmail.com>
2023-04-11 09:26:48 -0700

    chore: update year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mx-auto mt-auto w-full max-w-[65ch] px-[14px] pb-[14px] sm:px-0 sm:pb-8">
       <div className="border-t-[1px] border-zinc-500 text-zinc-500 transition-colors dark:border-zinc-400 dark:text-zinc-400">
         <div className="sm:float-left">
-          © 2022 by Nathen Smith. All rights reserved.
+          © 2023 by Nathen Smith. All rights reserved.
         </div>
         <a
           className="font-medium text-black underline transition-colors dark:text-white sm:float-right"
